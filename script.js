36949c41bbd722c728ef534eff5224dbd243edbe
Sushanta Biswas <bwsushanta@gmail.com>
2025-10-15 19:56:11 +0530

    I am done for today ig

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("Hello, World!"); // Not really my first js code lol.
+console.log("This is a sample JavaScript file.");
+console.log("I like pizzas a lot haha :D");
+
+// This is just a test file to do whatever I want :P
+
+window.alert("What's up?"); // Just for fun :D
+document.getElementById("words").innerText = "I love JavaScript!";
+document.getElementById("para").innerText = "JavaScript is a versatile programming language used for web development.";
