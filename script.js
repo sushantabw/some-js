c3dc297ccfaaafa12583232c6d3da66b208281e7
Sushanta Biswas <bwsushanta@gmail.com>
2025-10-16 10:43:17 +0530

    Learned variables and arithmatic operators

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-console.log("Hello, World!"); // Not really my first js code lol.
+// Not really my first js code lol.
+
+console.log("Hello, World!"); 
 console.log("This is a sample JavaScript file.");
 console.log("I like pizzas a lot haha :D");
 
@@ -7,3 +9,37 @@ console.log("I like pizzas a lot haha :D");
 window.alert("What's up?"); // Just for fun :D
 document.getElementById("words").innerText = "I love JavaScript!";
 document.getElementById("para").innerText = "JavaScript is a versatile programming language used for web development.";
+
+// Variables
+
+let name = "Alice";
+const age = 30;
+var city = "New York";
+
+console.log(name, age, city);
+console.log(`My name is ${name}, and I am ${age}, and I am from ${city}`);
+
+console.log("This is the end of the file.");
+
+document.getElementById("h1").textContent = "I am learning JavaScript!";
+document.getElementById("p1").textContent = "JavaScript is fun and powerful.";
+
+// Agumented arithmetic operators and arithmetic operators
+
+let x = 10;
+console.log(x);
+
+x += 5; // x = x + 5
+console.log(x);
+
+x -= 3; // x = x - 3
+console.log(x);
+
+x *= 2; // x = x * 2
+console.log(x);
+
+x /= 4; // x = x / 4
+console.log(x);
+
+x %= 3; // x = x % 3
+console.log(x);
